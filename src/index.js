d57b6ffdd5ff6e7cb2195e466670ab22070bc26d
Aaron Elias <aaronelias007@gmail.com>
2022-02-19 22:20:06 -0500

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("hello")
+//Varaibles
+
+const baseUrlFilter = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php'
+const baseUrlSearch = 'https://www.thecocktaildb.com/api/json/v1/1/search.php'
+
+const drinkCollection = document.querySelector('#drink-collection')
+
+
+const searchDrinks = (userInput) => {
+    const url = baseUrlSearch + '?s=' + userInput
+    console.log(url)
